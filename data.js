1200bea3767258a622dc1b5cae3a3667a62a2185
luispin0upv <lespjim@teleco.upv.es>
2026-05-14 19:57:03 +0200

    Corregir referencia de imagen Suecia visitante

--- data.js
+++ data.js
@@ -6,11 +6,32 @@ const productos = [
     imagen: "Camisetas/Alemania_camiseta.jpg",
     nuevo: true
   },
+    {
+    marca: "CLUB ELEVEN",
+    nombre: "Camiseta Alemania Visitante 2026",
+    precio: "$60",
+    imagen: "Camisetas/Alemania2da_camiseta.jpg",
+    nuevo: true
+  },
   {
     marca: "CLUB ELEVEN",
     nombre: "Camiseta México Local 2026",
     precio: "$60",
     imagen: "Camisetas/Mexico_camiseta.jpg",
+    nuevo: true
+  },
+  {
+      marca: "CLUB ELEVEN",
+    nombre: "Camiseta México Visitante 2026",
+    precio: "$60",
+    imagen: "Camisetas/Mexico2da_camiseta.jpg",
+    nuevo: true
+  },
+    {
+      marca: "CLUB ELEVEN",
+    nombre: "Camiseta México 3era Equipacion 2026",
+    precio: "$60",
+    imagen: "Camisetas/Mexico3era_camiseta.jpg",
     nuevo: true
   },
     {
@@ -57,9 +78,22 @@ const productos = [
   },
         {
     marca: "CLUB ELEVEN",
-    nombre: "Camiseta Japón Visitante 2026",
+    nombre: "Camiseta Suecia Local 2026",
     precio: "$60",
-    imagen: "Camisetas/Japon2da_camiseta.jpg",
+    imagen: "Camisetas/Suecia_camiseta.jpg",
+    nuevo: true
+  },
+          {
+    marca: "CLUB ELEVEN",
+    nombre: "Camiseta Suecia Visitante 2026",
+    precio: "$60",
+    imagen: "Camisetas/Suecia2da_camiseta.jpg",
+    nuevo: true
+  },        {
+    marca: "CLUB ELEVEN",
+    nombre: "Camiseta Venezuela Local 2026",
+    precio: "$60",
+    imagen: "Camisetas/Venezuela_camiseta.jpg",
     nuevo: true
   },
 ];
